6b1cf3863fe09e3cf190949bcfea9a653e67ffb4
DiAndyW <fangwanggo@outlook.com>
2025-07-02 02:19:34 -0700

    initial commit

---
+++ app/components/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <main>
+            <div className="flex p-4">
+                Andy Wang's Portfolio
+            </div>
+        </main>
+    );
+}
---
+++ app/welcome/welcome.tsx
@@ -0,0 +1,9 @@
+export function Welcome() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <div>
+        hello! this is my portfolio website
+      </div>
+    </main>
+  );
+}

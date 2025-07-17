3b0f515665617c25e7b3415a425ae07fd01058cd
DiAndyW <fangwanggo@outlook.com>
2025-07-16 23:01:20 -0700

    Added extended description if clicked

--- app/components/experienceTimeline.tsx
+++ app/components/experienceTimeline.tsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 
 const ExpTimeline = () => {
+    const [selectedItem, setSelectedItem] = useState<number | null>(null);
 
     const Content = [
         {
-            id: 0,
+            id: 1,
             time: "Mar 2025 - Present",
             title: "Fullstack Developer",
             organization: "Creative Labs",
             technologies: ['React', 'Next.js', 'Vercel', 'Supabase', 'TailwindCSS'],
-            description: [
-                "Worked with a 15-person team to build a website using React and Next.js to help UCLA students review and discover clubs",
+            description: "Worked with a 15-person team to build a website using React and Next.js to help UCLA students review and discover clubs",
+            points: [
                 "Helped create multiple dynamic API routes to Supabase for different queries to ensure efficient database usage",
                 "Implemented and optimized partial matching in search results, reducing loading times by 50%",
                 "Collaborated with the design team and other developers to bring Hi-Fi designs to life using TailwindCSS"
@@ -18,13 +19,13 @@ const ExpTimeline = () => {
 
         },
         {
-            id: 1,
+            id: 2,
             time: "Jan 2025 - Present",
             title: "Undergraduate Researcher",
             organization: "UCLA Department of Mathematics Lab",
             technologies: [],
-            description: [
-                "Collaborated with fellow researchers to conduct research on the Bandits Problems in a cascading multiplayer setting with information asymmetry",
+            description: "Collaborated with fellow researchers to conduct research on the Bandits Problems in a cascading multiplayer setting with information asymmetry",
+            points: [
                 "Numerically analyzed several other UCB algorithms for their effectiveness in the aforementioned problem settings to develop a more optimal approach ",
                 "Designed an algorithm involving an interval approach to create a list of the top K items, reaching sublinear regret"
             ]
@@ -38,8 +39,8 @@ const ExpTimeline = () => {
             <div className="relative">
                 <div className="absolute top-0 left-4 bottom-0 w-0.5 bg-gray-300"></div>
 
-                {Content.map((item, index) => (
-                    <div className="relative mb-10">
+                {Content.map((item) => (
+                    <div key={item.id} className="relative mb-10">
                         {/* Line */}
                         <div className="absolute left-3 top-6 w-3 h-3 bg-blue-500 rounded-full border-2 border-white z-10"></div>
 
@@ -49,22 +50,18 @@ const ExpTimeline = () => {
                             </h2>
 
                             {/* Description Card */}
-                            <h2 className="col-span-4 col-end-7 border rounded-lg p-4">
+                            <h2 className="col-span-4 col-end-7 border rounded-lg p-4 cursor-pointer" onClick={() => setSelectedItem(selectedItem === item.id ? null : item.id)}>
                                 <h3 className="font-bold mb-2 text-xl">
                                     {item.title}
                                 </h3>
                                 <h3 className="font-bold mb-2">
                                     {item.organization}
                                 </h3>
-
-                                {/* Bullet points */}
-                                <h3 className="mb-4">
-                                    {item.description.map(item => (
-                                        <p className="text-sm">
-                                            - {item}
-                                        </p>
-                                    ))}
+                                <h3 className="mb-4 text-md">
+                                    {item.description}
                                 </h3>
+
+                                {/* Technologies */}
                                 <h3 className="flex flex-wrap gap-2">
                                     {item.technologies.map(technology => (
                                         <div className="border rounded-full text-sm px-3 py-1">
@@ -72,6 +69,19 @@ const ExpTimeline = () => {
                                         </div>
                                     ))}
                                 </h3>
+                                
+                                {/* Extended description if card clicked */}
+                                {selectedItem == item.id && (
+                                    <div>
+                                        <h3 className="border my-4"></h3>
+                                        {item.points.map(item => (
+                                            <div className="text-sm flex">
+                                                <span className="pr-2">•</span>
+                                                {item}
+                                            </div>
+                                        ))}
+                                    </div>
+                                )}
                             </h2>
                         </div>
                     </div>
